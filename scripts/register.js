c2832cae822285681602a7f112e8c20de8f11ad6
roblack66 <romeoasante66@gmail.com>
2024-09-16 11:40:59 +0000

    First session on Javascript

---
+++ scripts/register.js
@@ -0,0 +1,10 @@
+// select form by id
+const registerForm = document.getElementById('register-form');
+// handle form submit event
+registerForm.onsubmit = function(){
+// collect username
+// collect email
+// collect password
+// save user information
+// send confirmation email
+}
